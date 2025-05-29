a9d74077bfbd3179c4173ffc8a41504bb37644cd
dexslender <dexslender@proton.me>
2025-05-29 16:23:15 -0600

    add(flake): simple flake.nix for nix devShell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# flake.nix
+{
+	description = "Simple flake for Orb";
+	
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+	};
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (
+			system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      goCore = with pkgs; [
+        go
+				gopls
+				go-tools
+        # delve
+			];
+    in {
+      devShell = pkgs.mkShell {
+        packages = goCore;
+        # shellHook = ''
+        #   export MI_VARIABLE="hola mundo"
+        # '';
+      };
+    }
+	);
+}
