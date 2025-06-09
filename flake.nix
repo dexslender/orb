1a8509e009f14a58e07cef872f5bfc70c50fee60
dexslender <dexslender@proton.me>
2025-06-08 23:09:55 -0600

    chore(flake.nix): Code reformat [indent issues]

--- flake.nix
+++ flake.nix
@@ -1,32 +1,31 @@
 # flake.nix
 {
-  description = "Simple flake for Orb";
-	
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-  };
+    description = "Simple flake for Orb";
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (
-      system:
-    let
-      # pkgs = nixpkgs.legacyPackages.${system};
-      pkgs = import nixpkgs { inherit system; };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        utils.url = "github:numtide/flake-utils";
+    };
 
-      goCore = with pkgs; [
-        go
-	gopls
-	go-tools
-        # delve
-      ];
-    in {
-      devShell = pkgs.mkShell {
-        packages = goCore;
-        # shellHook = ''
-        #   export MI_VARIABLE="hola mundo"
-        # '';
-      };
-    }
-  );
+    outputs = { self, nixpkgs, utils }:
+        utils.lib.eachDefaultSystem (
+            system:
+        let
+            # pkgs = nixpkgs.legacyPackages.${system};
+            pkgs = import nixpkgs { inherit system; };
+            gotools = with pkgs; [
+                go
+                gopls
+                go-tools
+                # delve
+            ];
+        in {
+            devShell = pkgs.mkShell {
+                packages = gotools;
+                # shellHook = ''
+                    #   export MY_VAR="hello world"
+                # '';
+            };
+        }
+    );
 }
